aec355e0affec515f75d48677c0a60a1e3787e72
Moh <mohakemal9@gmail.com>
2017-06-12 06:42:04 -0400

    first commit

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-"moh"
+// dependencies
+var request = require("request");
+var cheerio = require("cheerio");
+// requesting the web page
+request("www.usatoday.com", function(error, response, http){
+  // assigning the cheerio to $
+  var $ = cheerio.load(http);
+  // creating empty variable called result 
+  var result  = [];
+  // go to each 
+  $("p4.tittle).each(function(i, element){
+    
+    });
+});
