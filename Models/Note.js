81fe6449d0e7087a85ea0dae186ecb3af5f4d492
Moh <mohakemal9@gmail.com>
2017-06-14 03:08:53 -0400

    note created

--- Models/Note.js
+++ Models/Note.js
@@ -1,1 +1,19 @@
-﻿aa
+// Require mongoose
+var mongoose = require("mongoose");
+// create schema class
+var Schema = mongoose.Schema;
+// Create the note schema
+var NoteSchema = New Schema({
+  // just string
+  title: {
+    type: String
+  },
+  // just a string
+  body: {
+    type: String
+  }
+});
+//create note model with the NoteSchema
+var Note = mongoose.model("Note", NoteSchema);
+//export the note model
+module.exports = Note;
