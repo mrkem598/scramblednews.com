3ca7b01f412d4b414ed4c33565971b4425ba6253
Moh <mohakemal9@gmail.com>
2017-06-14 03:16:17 -0400

    Create Article.js

--- Models/Article.js
+++ Models/Article.js
@@ -1,1 +1,15 @@
-﻿
+// Require mongoose
+var mongoose = require("mongoose");
+// create schema class
+var Schema = mongoose.Schema;
+//Create article schema
+var ArticleSchema = new Schema({
+title: {
+  type: String,
+  required: true
+},
+  link: {
+    type: String,
+    require: true
+  },
+  ///saves only one note's objectId, ref refers to note model
